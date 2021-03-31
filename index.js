3b01b60b78818831422b7813463edd409d7be624
Augusto Parizio <augustoparizio.eng@gmail.com>
2021-03-31 10:17:10 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const { request, response } = require('express');
 const express = require('express');
-
 const app = express();
 
-console.log (app);
+//console.log (app);
+
+app.get('/',(request,response) => {
+    response.send('Olá,Mundo!');
+})
+
+app.listen(3000,() => {
+    console.log('servidor rodando');
+})
